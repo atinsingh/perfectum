6b4bfa791199bc3f6697ca6330cf9f06ead6e094
Atin Singh <atin@pragra.io>
2024-11-28 12:36:49 -0500

    launch page

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,21 +9,16 @@ type Props = {
 const Layout = ({ children, title = "This is the default title" }: Props) => (
   <div className="relative min-h-screen flex flex-col justify-center items-center bg-hero bg-cover">
     <Head>
-      <title>{title} | Themeptation</title>
+      <title>{title} | Perfectum AI</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <meta
-        name="description"
-        content="Slioth - is a One Page Saas Lading Page build with Tailwind CSS and NextJs created by themeptation"
-      />
-      <meta property="og:title" content={`${title} | Themeptation`} />
-      <meta
-        property="og:description"
-        content="Slioth - is a One Page Saas Lading Page build with Tailwind CSS and NextJs created by themeptation"
-      />
-      <meta property="og:url" content="https://slioth.themepttation.net/" />
+      <meta name="description" content="" />
+      <meta property="og:title" content={`${title} | Perfectum AI`} />
+      <meta property="og:description" content="" />
+      <meta property="og:url" content="https://perfectum.ai" />
       <meta property="og:type" content="website" />
     </Head>
+
     {children}
   </div>
 );
